02322958d08cb75ec59c6da54f330c0f15cf75dd
Jacob Groover <39018657+JacobGroover@users.noreply.github.com>
2024-02-10 17:59:38 -0500

    Adding res root folder for image resources etc.

--- Client/src/com/UGAHacks/main/Main.java
+++ Client/src/com/UGAHacks/main/Main.java
@@ -1,9 +1,11 @@
 package com.UGAHacks.main;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         // Server's hostname or IP. Use "localhost" if the server is running on the same machine
-        String host = "localhost";
+        String host = "172.21.67.47";
         int port = 8080; // Server port number
 
         Client client = new Client(host, port);
